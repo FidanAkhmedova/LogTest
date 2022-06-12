b4f69a4ab6551feb7131ba71a507d070a2c6fb85
FidanAkhmedova <fidan.akhmed.anar98@gmail.com>
2022-06-12 04:51:09 +0400

    1. Добавить на серверную сторону чата логирование, с выводом информации о действиях на сервере (запущен, произошла ошибка, клиент подключился, клиент прислал сообщение/команду). 2. Написать метод, которому в качестве аргумента передается не пустой одномерный целочисленный массив. Метод должен вернуть новый массив, который получен путем вытаскивания из исходного массива элементов, идущих после последней четверки. Входной массив должен содержать хотя бы одну четверку, иначе в методе необходимо выбросить RuntimeException. Написать набор тестов для этого метода (по 3-4 варианта входных данных). Вх: [ 1 2 4 4 2 3 4 1 7 ] -> вых: [ 1 7 ]. 3. Написать метод, который проверяет состав массива из чисел 1 и 4. Если в нем нет хоть одной четверки или единицы, то метод вернет false; Написать набор тестов для этого метода (по 3-4 варианта входных данных).

--- src/test/java/TestNumbers.java
+++ src/test/java/TestNumbers.java
@@ -2,50 +2,50 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class CalcTest {
-    private Calculator calculator;
+public class TestNumbers {
+    private Testing testing;
     public int[][] arr = {{1, 3, 2, 4, 5, 3, 6},{1, 2, 4, 4, 2, 3, 4, 1, 7},{1, 4, 4, 1, 1, 4, 3},{7, 5, 12, 4, 5, 3, 2},{12, 12, 24, 4 , 7, 2, 5, 8, 9, 10},{1, 3, 2, 4, 5, 3, 6, 4}};
     public int [][] arrayToCheck = {{5, 3, 6},{1, 7},{3},{5, 3, 2},{7, 2, 5, 8, 9, 10},{}};
 
     @BeforeEach
     public void init() {
-        calculator = new Calculator();
+        testing = new Testing();
     }
 
     @Test
     public void testEqual1() {
         int[] nums = arr[0];
-        Assertions.assertArrayEquals(arrayToCheck[0], calculator.arrTest(nums) );
+        Assertions.assertArrayEquals(arrayToCheck[0], testing.arrTest(nums) );
     }
     @Test
     public void testEqual2() {
         int[] nums = arr[1];
-        Assertions.assertArrayEquals(arrayToCheck[1], calculator.arrTest(nums) );
+        Assertions.assertArrayEquals(arrayToCheck[1], testing.arrTest(nums) );
     }
     @Test
     public void testEqual3() {
         int[] nums = arr[2];
-        Assertions.assertArrayEquals(arrayToCheck[2], calculator.arrTest(nums) );
+        Assertions.assertArrayEquals(arrayToCheck[2], testing.arrTest(nums) );
     }
     @Test
     public void testEqual4() {
         int[] nums = arr[3];
-        Assertions.assertArrayEquals(arrayToCheck[3], calculator.arrTest(nums) );
+        Assertions.assertArrayEquals(arrayToCheck[3], testing.arrTest(nums) );
     }
     @Test
     public void containsNumbersTest1() {
-        Assertions.assertEquals(true, calculator.containsNumbers(arr[0]));
+        Assertions.assertEquals(true, testing.containsNumbers(arr[0]));
     }
     @Test
     public void containsNumbersTest2() {
-        Assertions.assertEquals(false, calculator.containsNumbers(arrayToCheck[0]));
+        Assertions.assertEquals(false, testing.containsNumbers(arrayToCheck[0]));
     }
     @Test
     public void containsNumbersTest3() {
-        Assertions.assertEquals(false, calculator.containsNumbers(arrayToCheck[5]));
+        Assertions.assertEquals(false, testing.containsNumbers(arrayToCheck[5]));
     }
     @Test
     public void containsNumbersTest4() {
-        Assertions.assertEquals(true, calculator.containsNumbers(arr[1]));
+        Assertions.assertEquals(true, testing.containsNumbers(arr[1]));
     }
 }
